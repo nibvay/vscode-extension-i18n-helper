e8d3e5d545c137df90d1ec21698be0cc0bce732d
Ivy Fang <nibvay06@gmail.com>
2024-06-05 16:02:10 +0800

    fix: key format support double quotes

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # i18n-helper README
-
-
 ## Features
 
 This extension can help you view i18n translation when hover i18n key. 
 
+## How to try the this extension?
+1. Package
+```bash
+$ vsce package # i18n-helper-1.0.0.vsix is generated
+```
+2. Go to vscode side bar "Extension" --> Views and More Actions...
+3. Instal from VSIX...
+4. Select the .vsix file
+
 ## Requirements
-
 Set locales path.
-
-## Known Issues
-
-*  Haven't support i18n key with arguments. For example: `t('country-name-${arg}')`.
-
-## Todo list
-  - [] support i18n with arguments. 
 
 ---
 
--- src/i18nHoverProvider.ts
+++ src/i18nHoverProvider.ts
@@ -27,15 +27,14 @@ class I18nHoverProvider implements vscode.HoverProvider {
       const withTFuncRange = new vscode.Range(selection.start.line, selection.start.character - 3, selection.end.line, selection.end.character + 2);
       const highlightedWithTFunc = editor.document.getText(withTFuncRange);
 
-      const validTPrefix = [`t('`, `t(\``];
-      const validPostfix = [`')`, `\`)`];
+      const validTPrefix = [`t('`, `t(\``, `t("`];
+      const validPostfix = [`')`, `\`)`, `")`];
       if (
         validTPrefix.some(prefix => highlightedWithTFunc.slice(0, 3).includes(prefix)) && 
         validPostfix.some(postfix => highlightedWithTFunc.slice(-2,).includes(postfix))
       ) {
         // console.log('highlightedWithTFunc', highlightedWithTFunc);
         highlighted = editor.document.getText(selectionRange);
-        // console.log("highlighted word", highlighted);
         const contents = new vscode.MarkdownString(`<h4>[i18n-helper]</h4><span style="color:#fff;background-color:#395858;">${Languages['en-US']}</span>&nbsp;<span>${this.enLocaleMap.get(highlighted) ?? '---' }</span></br><span style="color:#fff;background-color:#395858;">${Languages['zh-TW']}</span>&nbsp;<span>${this.twLocaleMap.get(highlighted) ?? '---' }</span></br><span style="color:#fff;background-color:#395858;">${Languages['zh-CN']}</span>&nbsp;<span>${this.cnLocaleMap.get(highlighted) ?? '---' }</span>`);
         contents.isTrusted = true;
         contents.supportHtml = true;
